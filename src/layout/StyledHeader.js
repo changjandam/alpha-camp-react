725efa7f64d738c36a2cf9ed4a5b1f402ee51954
changjandamtw <changjandamtw@gmail.com>
2021-12-21 14:12:07 +0800

    add logo component

--- src/layout/StyledHeader.js
+++ src/layout/StyledHeader.js
@@ -1,16 +1,27 @@
 import styled from 'styled-components';
 import Hamburger from '../components/Hamburger';
+import Logo from '../components/Logo';
 
 const Header = styled.header`
   height: 56px;
   display: flex;
   align-items: center;
+  position: fixed;
+  top: 0;
+  width: 100vw;
 `;
+
+const TopLogo = styled(Logo)`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+`
 
 const StyledHeader = () => {
   return (
     <Header>
       <Hamburger />
+      <TopLogo />
     </Header>
   );
 };
