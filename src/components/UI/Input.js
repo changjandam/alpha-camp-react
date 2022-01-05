246982faba3822139d4ce5695b8ed2f11306f864
changjandamtw <changjandamtw@gmail.com>
2022-01-06 00:56:52 +0800

    prepare for chakra

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
 const Div = styled.div`
   margin: 1rem;
 `;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react';
+import colors from './theme'
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
+    <ChakraProvider theme={colors}>
       <App />
+    </ChakraProvider>
     {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
