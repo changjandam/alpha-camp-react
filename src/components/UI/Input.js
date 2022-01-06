9da8c034483ad3aa7d6ee0725a2d5d1c76b34c0a
changjandamtw <changjandamtw@gmail.com>
2022-01-06 17:18:19 +0800

    address form data and style done

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -3,45 +3,54 @@ import styled from 'styled-components';
 import { Select, Input as ChakraInput, Box } from '@chakra-ui/react';
 import { IoMdArrowDropdown } from 'react-icons/io';
 
-
 const Label = styled.label`
-	color: #808080;
-	display: block;
-	font-size: 12px;
-	margin-bottom: 0.5rem;
-	font-weight: bold;
+  color: #808080;
+  font-size: 12px;
+  margin-bottom: 0.5rem;
+  font-weight: bold;
 `;
 
+const Option = styled.option``;
+
 const typeSelector = (props) => {
-	switch (props.tagType) {
-		case 'input':
-			return (
-				<ChakraInput
-					id={props.id}
-					placeholder={props.placeholder}
-					type={props.type}
-					focusBorderColor="#f67599"
-					
-				/>
-			);
-		case 'select':
-			return (
-				<Select icon={<IoMdArrowDropdown />} focusBorderColor="#f67599" w={props.w}>
-					{props.options.map((option) => (
-						<option value={option.value}>{option.title}</option>
-					))}
-				</Select>
-			);
-		default:
-			return new Error();
-	}
+  switch (props.tagType) {
+    case 'input':
+      return (
+        <ChakraInput
+          id={props.id}
+          placeholder={props.placeholder}
+          type={props.type}
+          focusBorderColor='#f67599'
+          _placeholder={props.placeholder}
+        />
+      );
+    case 'select':
+      return (
+        <Select
+          icon={<IoMdArrowDropdown />}
+          focusBorderColor='#f67599'
+          w={props.w}
+          _placeholder={props.placeholder}
+        >
+          {props.options
+            ? props.options.map((option) => (
+                <option value={option.value} key={option.value}>
+                  {option.title}
+                </option>
+              ))
+            : ''}
+        </Select>
+      );
+    default:
+      return new Error();
+  }
 };
 
 export default function Input(props) {
-	return (
-		<Box pr={props.pr} mb={'1rem'}>
-			<Label for={props.id}>{props.title}</Label>
-			{typeSelector(props)}
-		</Box>
-	);
+  return (
+    <Box pr={props.pr} mb={'1rem'}>
+      <Label htmlFor={props.id}>{props.title}</Label>
+      {typeSelector(props)}
+    </Box>
+  );
 }
--- src/components/main/Address.js
+++ src/components/main/Address.js
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+
+import { XMLParser } from 'fast-xml-parser';
 
 import Input from '../UI/Input';
 
 import { Grid } from '@chakra-ui/react';
 
 const Form = styled.form`
-	margin: 15px;
+  margin: 15px;
 `;
 
 const genderOptions = [
-	{ title: '先生', value: 'male' },
-	{ title: '女士', value: 'female' },
+  { title: '先生', value: 'male' },
+  { title: '女士', value: 'female' },
 ];
 
 export default function Address() {
-	return (
-		<Form>
-			<Grid templateColumns={'1fr 1fr'}>
-				<Input tagType="input" id="name" title="姓名" pr={'30'} />
-				<Input
-					tagType="select"
-					title="稱謂"
-					id="gender"
-					options={genderOptions}
-				/>
-			</Grid>
-      <Input tagType="input" id="tel" title="電話" type='tel' />
-      <Input tagType="input" id="email" title="Email" type='email' />
-      {/* <Input tagType="select" id="city" title="縣市" /> */}
-      <Input tagType="input" id="address" title="地址" />
-		</Form>
-	);
+	const [counties, setCounties] = useState([]);
+  const getCounties = async () => {
+    const parser = new XMLParser();
+    const res = await fetch('https://api.nlsc.gov.tw/other/ListCounty');
+    const data = await res.text();
+    const counties =parser.parse(data).countyItems.countyItem.map((item) => ({
+      title: item.countyname,
+      value: item.countyname,
+    }));
+    setCounties(counties)
+  };
+
+  useEffect(() => {
+    getCounties();    
+  }, []);
+
+  return (
+    <Form>
+      <Grid templateColumns={'1fr 1fr'}>
+        <Input
+          tagType='select'
+          title='稱謂'
+          id='gender'
+          options={genderOptions}
+          pr={'30'}
+        />
+        <Input tagType='input' id='name' title='姓名' placeholder='請輸入姓名' />
+      </Grid>
+      <Input tagType='input' id='tel' title='電話' type='tel' placeholder='請輸入行動電話' />
+      <Input tagType='input' id='email' title='Email' type='email' placeholder='請輸入電子郵件' />
+      <Input tagType='select' id='city' title='縣市' options={counties} placeholder='請選擇縣市'/>
+      <Input tagType='input' id='address' title='地址' placeholder='請輸入地址' />
+    </Form>
+  );
 }
